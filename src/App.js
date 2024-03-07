bf39a862e1d0e0b15c03bec01c1ce8fb813f4d68
Rohit Phalke <rohitphalke165@gmail.com>
2024-03-07 22:28:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Quote from './QuoteSection';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Quote/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
